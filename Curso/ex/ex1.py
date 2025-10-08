b7e0db47e0c911744d0e6b2730beedf93f256a26
= <cavaleiromiau@gmail.com>
2025-10-07 21:09:15 -0300

    t

--- Curso/ex/ex1.py
+++ Curso/ex/ex1.py
@@ -1,1 +1,17 @@
-frase = str(input("Digite uma frasse:"))
+import string
+from collections import Counter
+
+
+frase = input("Digite uma frasse:").lower()
+contagem = {}
+
+for letra in frase:
+    if letra in string.ascii_lowercase:
+        if letra in contagem:
+            contagem[letra] += 1
+        else:
+            contagem[letra] = 1 
+print("\nContagem final:")
+print(contagem)
+
+#if .isalpha(frase):
