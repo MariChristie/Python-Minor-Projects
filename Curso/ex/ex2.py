d71b729f973e0024b5b19950f02fbe90efe80abe
= <cavaleiromiau@gmail.com>
2025-10-09 19:59:12 -0300

    d

--- Curso/ex/ex2.py
+++ Curso/ex/ex2.py
@@ -1,5 +1,23 @@
 pedir = input("Digite seu CPF: ")
+cpf_nove_digitos = pedir[0:9]
+soma_digito1 = 0
+
+
 
 if len(pedir) == 11:
-    print("CPF válido")
+    if len(set(pedir)) ==1:
+        print("CPF inválido - todos os números iguais")
+    else:
+        for indice, digito in enumerate(cpf_nove_digitos):
+            multiplicador = 10 - indice
+            soma_digito1 += int(digito) * multiplicador
+        resto = (soma_digito1 * 10) % 11
+        if resto == 10:
+            digito_calculado = 0
+        else:
+            digito_calculado = resto
+        digito_original = int(pedir[9])
+
+            
 else: print("CPF inválido")
+
